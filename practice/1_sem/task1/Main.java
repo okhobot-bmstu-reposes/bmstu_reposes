a56ee9e8933b5728672db2f7ea59c68273daca45
okhobot <okhobot@users.noreply.github.com>
2025-12-01 14:01:44 +0300

    practice_1_sem_task1 add user interface (#5)

--- practice/1_sem/task1/Main.java
+++ practice/1_sem/task1/Main.java
@@ -1,4 +1,7 @@
 import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {  
     public static Integer findMax(List<Integer> numbers) {
@@ -14,4 +17,18 @@ public class Main {
         }
         return max;
     }
+    public static void main(String[] args) {  
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> numbers = new ArrayList<>();
+
+        System.out.println("Введите количество чисел: ");  
+        Integer n= Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Введите числа(каждое на новой строке):");  
+
+        for(int i=0;i<n;i++)
+            numbers.add(Integer.parseInt(scanner.nextLine()));
+        
+        System.out.println("Максимальное число: "+findMax(numbers));  
+    }  
 }  
