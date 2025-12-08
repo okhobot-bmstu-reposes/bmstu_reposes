8db9fe58ad8bff1451235822e8ebbba757a476d3
okhobot03 <okhobot03@gmail.com>
2025-12-08 13:22:05 +0300

    practice_1_sem_task2 add exceptions

--- practice/1_sem/task2/Rectangle.java
+++ practice/1_sem/task2/Rectangle.java
@@ -1,6 +1,7 @@
 public class Rectangle {  
     private float a,b;
     public Rectangle(float a, float b) {  
+        if(a<=0 || b<=0) throw new RuntimeException("There is no figure with such parameters.");
         this.a=a;
         this.b=b;
     }  
