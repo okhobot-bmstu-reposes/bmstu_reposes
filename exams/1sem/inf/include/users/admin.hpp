b5cdb73251fd6cd5116e4deb0e91862f2855df0c
okhobot03 <okhobot03@gmail.com>
2026-01-05 13:52:23 +0300

    1sem_inf_exam_task add SQL updateOrderStatus usage

--- exams/1sem/inf/include/users/admin.hpp
+++ exams/1sem/inf/include/users/admin.hpp
@@ -10,7 +10,7 @@ class Admin : public User
 public:
     void addProduct(Product p) { dbConn->executeNonQuery("INSERT INTO products (name, price, stock_quantity) VALUES " + p.getInsertString() + ";"); }
     void deleteProduct(int product_id) { dbConn->executeNonQuery("DELETE FROM products WHERE product_id=" + std::to_string(product_id) + ";"); }
-    void updateOrderStatus(int id, std::string new_status) { dbConn->executeNonQuery("UPDATE orders SET status=new_status WHERE irder_id=" + std::to_string(id) + ";"); }
+    void updateOrderStatus(int id, std::string new_status) { dbConn->executeNonQuery("CALL updateOrderStatus("+std::to_string(id)+", \'"+new_status+"\', 1);"); }
     void updateProduct(int id, unsigned int new_stock_quantity) { dbConn->executeNonQuery("UPDATE products SET stock_quantity=" + new_stock_quantity + " WHERE product_id="+id+";"); }
 
     void viewAllOrders()
--- exams/1sem/inf/include/users/manager.hpp
+++ exams/1sem/inf/include/users/manager.hpp
@@ -6,10 +6,15 @@
 #include <order.hpp>
 #include <payment.hpp>
 #include <users/user.hpp>
-class Manager:public User
+class Manager : public User
 {
 public:
-    void approveOrder(int id) { dbConn->executeNonQuery("UPDATE orders SET status=\'completed\' WHERE irder_id=" + std::to_string(id) + ";"); }
-    void updateStock(int id, unsigned int new_stock_quantity) { dbConn->executeNonQuery("UPDATE products SET stock_quantity=" + new_stock_quantity + " WHERE product_id="+id+";"); }
-
+    void approveOrder(int id)
+    {
+        dbConn->executeNonQuery("CALL updateOrderStatus(" + std::to_string(id) + ", \'completed\', 1);");
+    }
+    void updateStock(int id, unsigned int new_stock_quantity)
+    {
+        dbConn->executeNonQuery("UPDATE products SET stock_quantity=" + new_stock_quantity + " WHERE product_id=" + id + ";");
+    }
 };
